db188a83d4eefa5eb0cba31ee5c807dec3b8fd56
Izan Pérez Cosano <izanperez98@gmail.com>
2019-03-23 17:28:53 +0100

    fixed CompanyManager

--- src/Empresa/CompanyManager.java
+++ src/Empresa/CompanyManager.java
@@ -4,17 +4,10 @@ import java.util.List;
 
 public interface CompanyManager {
 
-<<<<<<< HEAD
-void nuevaventa(int idventa, int idvendedor);
-void addEmployee(String name, int id, double salary);
-=======
-    void addEmployee(String id, String name,  String type, double salary, String idManager);
-    void nuevaVenta(int idventa, int idvendedor);
-
+    void addSale(int id, int idVendor);
+    void addEmployee(String name, int id, double salary, String idManager);
     List<Employee> findAllOrderBySalary();
     List<Employee> findAllOrderByName();
     List<Employee> findAllByManager(String idManager);
->>>>>>> refs/remotes/origin/master
-
 
 }
